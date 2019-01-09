b5069b214aaf897ed27b8e06e3a76085e03acb16
chingaloEric <echingalo@gmail.com>
2019-01-09 20:36:37 +0300

    solved the prime number challenge

---
+++ primeNumbers.py
@@ -0,0 +1,15 @@
+# for testNumber in range(2, 10):
+#     for number in (2, testNumber):
+#         if testNumber % number == 0:
+#             print(testNumber, "is Not prime")
+#             break
+#     else:
+#         print(testNumber, "is prime")
+
+for testNumber in range(2, 10):
+    for number in range(2, testNumber):
+        if testNumber % number == 0:
+            print testNumber, "is NOT prime" 
+            break
+    else:
+        print testNumber, 'is prime'
