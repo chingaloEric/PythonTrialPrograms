9b14da2ccaa9cbbdf3514ea79bfb8a70652fcbe6
chingaloEric <echingalo@gmail.com>
2019-01-09 17:56:41 +0300

    first commit

---
+++ flowControl.py
@@ -0,0 +1,15 @@
+# while loop in python
+count = 0
+str = 'Eric'
+while count < len(str):
+    print(str[count]),
+    print('\n'),
+    count = count + 1
+print('ended the loop')
+    
+# if statement
+if len(str) < 3: 
+    print(str + ' is a Weak name')
+else:
+    print(str + ' is a Strong name')
+
