603e7d18b40ff7240104310747f0c07b34032618
chingaloEric <echingalo@gmail.com>
2019-01-10 20:20:21 +0300

    defined a simple python function

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,8 @@
 #Hello world program using range(function)
-for i in range(0, 10 , 1):
-    print(i+1),
-    print('Hello World!')
+# for i in range(0, 10 , 1):
+#     print i+1, 'Hello World!'
+
+def summation(a, b):
+    print a, '+', b, '=', a+b
+
+summation(10, 2)
