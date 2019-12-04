08834ecca7ec78178f8678c47df509f8cafb1cf1
chingaloEric <echingalo@gmail.com>
2019-12-04 21:10:08 +0300

    added list comprehension tutorials

--- list.py
+++ list.py
@@ -6,5 +6,17 @@ for x in range(10):
 
 # better approach
 newSquares = list(map(lambda x: x**2, range(11)))
-print(newSquares)
+# print(newSquares)
 
+just_list = [1, 'Eric']
+# print(len(just_list))
+
+names = ['Eric', 'Lisa']
+message = 'Hello'
+for count in range(len(names)):
+    # print(message + " " + names[count])
+    pass
+
+# list comprehensions
+squares = [num**2 for num in range(1,11)]
+print(squares)
