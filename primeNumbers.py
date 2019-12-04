2ca1af56223bce52afe4ae38ba39185ffad5830c
chingaloEric <echingalo@gmail.com>
2019-12-04 21:14:00 +0300

    modified the prime number generator

--- primeNumbers.py
+++ primeNumbers.py
@@ -9,7 +9,8 @@
 for testNumber in range(2, 10):
     for number in range(2, testNumber):
         if testNumber % number == 0:
-            print testNumber, "is NOT prime" 
-            break
+            pass
+            # print( testNumber, "is NOT prime" )
+            # break
     else:
-        print testNumber, 'is prime'
+        print(testNumber, 'is prime')
