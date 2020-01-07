f8671c7bd579d7eb2544d429b3058d10f5f36a1d
chingaloEric <echingalo@gmail.com>
2020-01-07 18:08:00 +0300

    feat: 🎸 added class instances as attributes

--- classes/Car.py
+++ classes/Car.py
@@ -17,9 +17,18 @@ class Car():
         self.model = model
 
 # Inhearitance
+class Battery():
+    def __init__(self, battery = 70):
+        self.battery = battery
+    
+    def get_battery_details(self):
+        return str(self.battery) + '-kwh'
+
 class ElectricCar(Car):
     def __init__(self, name, model, year):
         super().__init__(name, model, year)
+        self.battery = Battery(100)
 
 car_1 = ElectricCar('Tesla', 'Model X', 2020)
 print(car_1.get_descriptive_name())
+print('Battery power is: ' + car_1.battery.get_battery_details())
