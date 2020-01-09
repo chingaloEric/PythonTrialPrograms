9d7bbd06c85eca89f5d420328f22f49476a62717
chingaloEric <echingalo@gmail.com>
2020-01-09 20:50:45 +0300

    fix: 🐛 added the else block
    the else block works when try doesnt catch any exception

--- exceptions/division.py
+++ exceptions/division.py
@@ -5,7 +5,10 @@ When an exception is not handled, a trace back is created
 '''
 # try-catch block
 try:
-    print(5/0)
+    print(5/1)
 # if exception error is not specified, python checks all exceptions
 except ZeroDivisionError:
     print('Fool! You cant devide by zero')
+# else block works when there is no exception on the try block
+else: 
+    print('Well done!')
