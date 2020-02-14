8244582afacc415da88c865bb9e65968cb4799d4
chingaloEric <echingalo@gmail.com>
2020-02-14 12:08:56 +0300

    feat: 🎸 created sets

---
+++ sets.py
@@ -0,0 +1,9 @@
+'''
+Sets have the following properties:
+    1. Collection of items in unordered
+    2. No duplicate items can be stored
+    3. Sets are mutable
+'''
+languages = set()
+names = {'Eric', 'Lisa'}
+print(type)
