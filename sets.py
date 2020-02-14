674b8bc996431916db0432316c8bb9ee72e8dea4
chingaloEric <echingalo@gmail.com>
2020-02-14 13:05:46 +0300

    feat: 🎸 added set operations on multiple sets

--- sets.py
+++ sets.py
@@ -1,7 +1,7 @@
 '''
 Sets have the following properties:
-    1. Collection of items in unordered
-    2. No duplicate items can be stored
+    1. Collection of elements in unordered
+    2. No duplicate elements can be stored
     3. Sets are mutable
 '''
 # created sets
@@ -9,42 +9,86 @@ languages = set()
 names = {'Eric', 'Lisa'}
 print(names)
 
-# adding items to set
+# adding elements to set
 languages.add('Python')
 languages.add('Java')
+languages.add('php')
+# adding multiple elements
+languages.update(['C', 'Javascript'])
 
 '''
-removing specific item in set
+removing specific element in set
 '''
-# if an item specified is not found, the function does nothing
+# if an element specified is not found, the function does nothing
 languages.discard('Javaa')
-# if an item is not found raises keyError
+# if an element is not found raises keyError
 languages.remove('Java')
 
-# removing the last item of the set
+# removing the last element of the set
 languages.pop()
 print(languages)
 
-# removed all items in set
+# removed all elements in set
 languages.clear()
 
 # coping a set
 new_names = names.copy()
 print(new_names)
 
-# max item
+# max element
 num = {1, 3, 10, 2, 5}
 print(max(num))
 
-# min item
+# min element
 print(min(num))
 
-# number of items in the set
+# number of elements in the set
 print(len(num))
 
 # sort the set
 sorted_num = sorted(num)
 print(sorted_num)
 
-# sum of items
+# sum of elements
 print(sum(sorted_num))
+
+'''
+Set operation invloving multiple sets
+'''
+num_set_1 = set([1, 2, 30, 48, 10])
+num_set_2 = set([1, 10, 43, 55, 100, 39 , 13])
+
+# difference between sets
+# A.difference(B) returns element in A but not B
+print(num_set_2.difference(num_set_1))
+# find difference and update the set
+# num_set_2.difference_update(num_set_1)
+# print(num_set_2)
+
+# to find symmetric difference
+print(num_set_2.symmetric_difference(num_set_1))
+# find difference and update the set
+# num_set_2.symmetric_difference_update(num_set_1)
+# print(num_set_2)
+
+# to find intersection
+print(num_set_1.intersection(num_set_2))
+# to find intersection and update the set
+# num_set_1.intersection_update(num_set_2)
+# print(num_set_1)
+
+# to find union
+print(num_set_1.union(num_set_2))
+# to find intersection and update the set
+# num_set_1.update(num_set_2)
+# print(num_set_1)
+
+# check disjoint
+print(num_set_1.isdisjoint(num_set_2))
+
+# check subset
+print(num_set_1.issubset(num_set_2))
+
+# check superset
+print(num_set_1.issuperset(num_set_2))
+
